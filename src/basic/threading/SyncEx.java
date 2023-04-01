82bb08cdfe8c40c6ebf894f7bd9634bd1ad6fd73
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2023-04-01 12:41:28 +0530

    thread inconsistency example

---
+++ src/basic/threading/SyncEx.java
@@ -0,0 +1,65 @@
+package basic.threading;
+
+public class SyncEx {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter ic = new InventoryCounter();
+
+        IncrementingThread inc = new IncrementingThread(ic);
+        DecrementingThread dec = new DecrementingThread(ic);
+
+        inc.start();
+        dec.start();
+
+        inc.join();
+        dec.join();
+
+        System.out.println("We currently have " + ic.getItems() + " items");
+
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter ic;
+
+        public DecrementingThread(InventoryCounter ic) {
+            this.ic = ic;
+        }
+
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                ic.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter ic;
+
+        public IncrementingThread(InventoryCounter ic) {
+            this.ic = ic;
+        }
+
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                ic.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
+
+
